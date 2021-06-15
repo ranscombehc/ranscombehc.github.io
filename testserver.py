4c7186803369bb7705e1150c9bb0a7ffab54393c
True Ghiassi <true@ghiassitrio.co.uk>
2021-06-15 19:10:43 +0100

    All done but the video.

--- testserver.py
+++ testserver.py
@@ -4,11 +4,23 @@ import bottle
 def getmain():
     return bottle.static_file('index.html', root='.')
 
+@bottle.get('/fonts/alegreya/<filename>')
+def getAlegreya(filename):
+    return bottle.static_file(filename, root='./fonts/alegreya')
+
+@bottle.get('/fonts/greatvibes/<filename>')
+def getAlegreya(filename):
+    return bottle.static_file(filename, root='./fonts/greatvibes')
+
 @bottle.get('/<filename>')
 def getstyles(filename):
     if '.' not in filename:
         filename = filename + '.html'
     return bottle.static_file(filename, root='.')
+
+@bottle.get('/clickslideshow/<filename>')
+def slideshow(filename):
+    return bottle.static_file(filename, root='./clickslideshow')
 
 @bottle.get('/chapelTop/<filename>')
 def chapelTop(filename):
@@ -18,8 +30,20 @@ def chapelTop(filename):
 def chapelTop(filename):
     return bottle.static_file(filename, root='./chapelManual')
 
+@bottle.get('/topslideshow/<filename>')
+def topslideshow(filename):
+    return bottle.static_file(filename, root='./topslideshow')
+
+@bottle.get('/reviewPhotos/<filename>')
+def topslideshow(filename):
+    return bottle.static_file(filename, root='./reviewPhotos')
+
 @bottle.get('/localPhotos/<filename>')
-def chapelTop(filename):
+def topslideshow(filename):
     return bottle.static_file(filename, root='./localPhotos')
+
+@bottle.get('/roomPhotos/<filename>')
+def topslideshow(filename):
+    return bottle.static_file(filename, root='./roomPhotos')
 
 bottle.run(host='localhost', port=3000, debug=True, server='paste')
